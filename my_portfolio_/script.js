b6c5d9e472b1de4f59affbcbcf0bca0b9650ac82
ashoka <ashoklande73@gmail.com>
2026-02-13 18:53:35 +0530

    final commit

--- my_portfolio_/script.js
+++ my_portfolio_/script.js
@@ -1,34 +1,85 @@
-//email send template
+//cv button \
 
-  (function() {
-    emailjs.init("2tEjnkbPxMxE7SnaX");
-  })();
+const cvbutton = document.querySelector(".cv-button");
 
-  document.getElementById("contact-form")
-    .addEventListener("submit", function(event) {
-      event.preventDefault();
+cvbutton.addEventListener("click", () => {
+  window.open(
+    "https://portfolio-5we93bqtr-ashoklandes-projects.vercel.app/assets/Resume-CtYq5Sqm.pdf",
+    "_blank"
+  );
+});
 
-      emailjs.sendForm("service_14dkw7i", "template_xev7haj", this)
-        .then(function() {
-          alert("Message Sent Successfully!");
-        }, function(error) {
-          alert("Failed to send message");
-        });
+// navigation link function
+
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+navLinks.forEach((link) =>
+  link.addEventListener("click", function () {
+    const TargatedId = this.getAttribute("data-target");
+    const section = document.getElementById(TargatedId);
+
+    section.scrollIntoView({
+      behavior: "smooth",
     });
+  })
+);
+
+// scrolling function
+
+let lastScrollTop = 0;
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Change background after 20px scroll
+  if (currentScroll > 20) {
+    const targatedId = header.getElementsByTagName("a");
+    header.style.backgroundColor = "#ffd2c2";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+
+  if (currentScroll > lastScrollTop) {
+    // Scrolling Down → Hide header
+    header.style.top = "-100px";
+  } else {
+    // Scrolling Up → Show header
+    header.style.top = "0";
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+})(
+  //email send template
+
+  function () {
+    emailjs.init("2tEjnkbPxMxE7SnaX");
+  }
+)();
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    emailjs.sendForm("service_14dkw7i", "template_xev7haj", this).then(
+      function () {
+        alert("Message Sent Successfully!");
+      },
+      function (error) {
+        alert("Failed to send message");
+      }
+    );
+  });
 
 
+//social media link
 
-
-
-
-//social media link 
-
-function openinstagram(){
-    window.open("https://www.instagram.com/ashoklande73/?hl=en","_blank")
+function openinstagram() {
+  window.open("https://www.instagram.com/ashoklande73/?hl=en", "_blank");
 }
-function openfacebook(){
-    window.open("https://www.facebook.com","_blank")
+function openfacebook() {
+  window.open("https://www.facebook.com", "_blank");
 }
-function openlinkedin(){
-    window.open("https://www.linkedin.com/in/ashok-lande-492431230/","_blank")
+function openlinkedin() {
+  window.open("https://www.linkedin.com/in/ashok-lande-492431230/", "_blank");
 }
